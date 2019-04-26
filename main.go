978c9fcd524259c715e52567714842dfa3452f64
FahKing <fahkingnp@gmail.com>
2019-04-26 12:41:40 +0700

    modified .go server1

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ func sayHello(w http.ResponseWriter, r *http.Request) {
 func ConnectServer2(w http.ResponseWriter, r *http.Request) {
 	message := r.URL.Path
 	message = strings.TrimPrefix(message, "/")
-	url := "http://localhost:8000/ " + message
+	url := "http://godockertwo:8000/ " + message
 	resp, err := http.Get(url)
 	if err != nil {
 		panic(err)
@@ -38,9 +38,16 @@ func ConnectServer2(w http.ResponseWriter, r *http.Request) {
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	fmt.Println(string(body))
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(body)
 }
 
 func main() {
+	// http.HandleFunc("/", sayHello)
+	// if err := http.ListenAndServe(":8080", nil); err != nil {
+	// 	panic(err)
+	// }
 	http.HandleFunc("/", ConnectServer2)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		panic(err)
